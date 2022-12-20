0f0fc212b4204319d37810410abf622bf12254c5
mvtiwari <mayurvtiwari@gmail.com>
2022-12-20 15:48:57 +0530

    First change in Calc.py file

--- calc.py
+++ calc.py
@@ -1,15 +1,17 @@
 class Calculator:
-    def init():
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
+    def add(self):
+        print('The addition of ', self.a, ' and ', self.b, ' is ', self.a + self.b)
+    def sub(self):
         pass
-    def add():
+    def mul(self):
         pass
-    def sub():
-        pass
-    def mul():
-        pass
-    def div():
+    def div(self):
         pass
 
-
+a = int(input("Enter the First value : "))
+b = int(input("Enter second value : "))
 c = Calculator(a,b)
-
+c.add()

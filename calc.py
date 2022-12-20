c12f71db7b0e48ae87de06e2f65beffdc681ea0e
mayurentc <mayurentc@gmail.com>
2022-12-20 13:56:12 +0530

    First commit

---
+++ calc.py
@@ -0,0 +1,15 @@
+class Calculator:
+    def init():
+        pass
+    def add():
+        pass
+    def sub():
+        pass
+    def mul():
+        pass
+    def div():
+        pass
+
+
+c = Calculator(a,b)
+
---
+++ mul.py
@@ -0,0 +1,3 @@
+def mul(x,y):
+    pass
+    
